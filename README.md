f10dba6881aa680824d3e63ece1c962a00e99f1e
Stefano Taillefert <stefano.taille@gmail.com>
2021-01-30 00:54:45 +0100

    Add options, fix data fetch cases

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StonksBot
-A bot that LIKES THE STOCK
+## A bot that LIKES THE STOCK 🚀🚀
+
+Check the options at the beginning of `main.py` for some config like custom command or dedicated channel.
+
+To run it simply yeet the bot token in a system env var named `DISC_BOT_TOKEN` and then run `main.py`. Easy.
--- main.py
+++ main.py
@@ -7,23 +7,36 @@ import requests
 # Discord bot that prints stock prices
 # author: Stefano Taillefert
 
-# the text channel you want the bot to post in
+# The text channel you want the bot to post in
+channel_protection = True
 bot_channel = "bot-spam"
+
+# The command to trigger the bot (one word only)
+command = "$stonk"
 
 client = discord.Client()
 
 def get_quote(symbol):
   response = requests.get("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&symbols=" + symbol)
   json_data = json.loads(response.text)
+
   if len(json_data['quoteResponse']['result']) == 0:
-      return False
+    # Symbol not found
+    return False
+
   quote = json_data['quoteResponse']['result'][0]
   output = quote['symbol'] + ": "
+
   if quote['marketState'] == "PRE":
     output += "[PRE] " + str(quote['preMarketPrice']) + " ("
     if quote['preMarketChangePercent'] > 0:
         output += "+"
     output += str(round(quote['preMarketChangePercent'], 2)) + "%)"
+  elif quote['marketState'] == "POST":
+    output += "[POST] " + str(quote['postMarketPrice']) + " ("
+    if quote['postMarketChangePercent'] > 0:
+        output += "+"
+    output += str(round(quote['postMarketChangePercent'], 2)) + "%)"
   else:
     output += str(quote['regularMarketPrice']) + " (" 
     if quote['regularMarketChangePercent'] > 0:
@@ -37,18 +50,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  # Prevent messages from the bot itself
   if message.author == client.user:
     return
 
-  if str(message.channel) != str(bot_channel):
+  # If channel protection is enabled, check if right channel
+  if channel_protection and str(message.channel) != str(bot_channel):
     return
 
-  if message.content.startswith('$stonk') and len(message.content.split()) == 2:
-    answer = get_quote(message.content.split()[1])
-    if answer == False:
-      answer = "Error: unknown symbol" 
-  else:
+  # Listen only for correct commands
+  if not message.content.startswith(command):
+    return
+  
+  # Check that we get only one symbol
+  if len(message.content.split()) != 2:
     answer = "Error. Only one stock supported at the moment. Usage: `$stonk SYMBOL`"
+  else:
+    answer = get_quote(message.content.split()[1])
+
+    # Check that the symbol is actually valid
+    if answer == False:
+      answer = "Error: unknown symbol"
+
   await message.channel.send(answer)
 
 client.run(os.getenv('DISC_BOT_TOKEN'))
