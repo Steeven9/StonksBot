a053b991b114419c2b829a28d34eb3def6715d08
Stefano Taillefert <stefano.taille@gmail.com>
2021-11-03 18:31:59 +0100

    Add crypto support

--- main.py
+++ main.py
@@ -28,21 +28,44 @@ def get_quote(symbol):
     output = quote["symbol"] + ": "
 
     if quote["marketState"] == "PRE":
-        output += "[PRE] " + str(quote["preMarketPrice"]) + " ("
+        output += (
+            "[PRE] "
+            + str(quote["preMarketPrice"])
+            + " "
+            + str(quote["currency"])
+            + " ("
+        )
         if quote["preMarketChangePercent"] > 0:
             output += "+"
         output += str(round(quote["preMarketChangePercent"], 2)) + "%)"
     elif quote["marketState"] == "POST":
-        output += "[POST] " + str(quote["postMarketPrice"]) + " ("
+        output += (
+            "[POST] "
+            + str(quote["postMarketPrice"])
+            + " "
+            + str(quote["currency"])
+            + " ("
+        )
         if quote["postMarketChangePercent"] > 0:
             output += "+"
         output += str(round(quote["postMarketChangePercent"], 2)) + "%)"
     else:
-        output += str(quote["regularMarketPrice"]) + " ("
+        output += str(quote["regularMarketPrice"]) + " " + str(quote["currency"]) + " ("
         if quote["regularMarketChangePercent"] > 0:
             output += "+"
         output += str(round(quote["regularMarketChangePercent"], 2)) + "%)"
     return output
+
+
+def get_crypto_quote(symbol):
+    response = requests.get("https://stonks.soulsbros.ch/actions/cryptoApi.php")
+    json_data = json.loads(response.text)
+
+    if len(json_data[symbol]) == 0:
+        # Symbol not found
+        return "Crypto `" + symbol + "` not found"
+
+    return symbol.upper() + ": " + str(json_data[symbol]["USD"]) + " USD"
 
 
 @client.event
@@ -64,6 +87,22 @@ async def on_ready():
 )
 async def stock(ctx, ticker: str):
     await ctx.send(f"{get_quote(ticker)}")
+
+
+@slash.slash(
+    name="crypto",
+    description="Get the current price of a given cryptocurrency",
+    options=[
+        create_option(
+            name="symbol",
+            description="Crypto ticker (e.g. BTC)",
+            option_type=3,  # string
+            required=True,
+        )
+    ],
+)
+async def crypto(ctx, symbol: str):
+    await ctx.send(f"{get_crypto_quote(symbol)}")
 
 
 client.run(os.getenv("STONKSBOT_TOKEN"))
