f1554a332c2dd157f1bf9255f93a4a9676e4abaa
Stefano Taillefert <stefano.taille@gmail.com>
2021-11-03 13:46:20 +0100

    Rewrite with slash, add CI/CD

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ To install the requirements, run `pip install --no-cache-dir -r requirements.txt
 
 Check the options at the beginning of `main.py` for some options like custom command or dedicated channel.
 
-To run it, simply yeet the bot token in a system environment variable named `DISC_BOT_TOKEN` and then run `python main.py`. Easy.
+To run it, simply yeet the bot token in a system environment variable named `STONKSBOT_TOKEN` and then run `python main.py`. Easy.
 
 
 ## Usage
@@ -18,7 +18,7 @@ That's it. Literally.
 
 ## Docker? Docker!
 
-Create a `.env` file with the bot token as `DISC_BOT_TOKEN`, then build or pull the image and run it:
+Create a `.env` file with the bot token as `STONKSBOT_TOKEN`, then build or pull the image and run it:
 
 `docker build . -t stonksbot` or `docker pull steeven9/stonksbot`
 
--- main.py
+++ main.py
@@ -2,76 +2,68 @@ import json
 import os
 
 import discord
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 import requests
+
 
 # Discord bot that prints stock prices
 # author: Stefano Taillefert
 
-# The text channel you want the bot to post in
-channel_protection = True
-bot_channel = "bot-spam"
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True)
 
-# The command to trigger the bot (one word only)
-command = "$stonk"
-
-client = discord.Client()
 
 def get_quote(symbol):
-  response = requests.get("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&symbols=" + symbol)
-  json_data = json.loads(response.text)
+    response = requests.get(
+        "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&symbols=" + symbol
+    )
+    json_data = json.loads(response.text)
 
-  if len(json_data['quoteResponse']['result']) == 0:
-    # Symbol not found
-    return False
+    if len(json_data["quoteResponse"]["result"]) == 0:
+        # Symbol not found
+        return False
 
-  quote = json_data['quoteResponse']['result'][0]
-  output = quote['symbol'] + ": "
+    quote = json_data["quoteResponse"]["result"][0]
+    output = quote["symbol"] + ": "
 
-  if quote['marketState'] == "PRE":
-    output += "[PRE] " + str(quote['preMarketPrice']) + " ("
-    if quote['preMarketChangePercent'] > 0:
-        output += "+"
-    output += str(round(quote['preMarketChangePercent'], 2)) + "%)"
-  elif quote['marketState'] == "POST":
-    output += "[POST] " + str(quote['postMarketPrice']) + " ("
-    if quote['postMarketChangePercent'] > 0:
-        output += "+"
-    output += str(round(quote['postMarketChangePercent'], 2)) + "%)"
-  else:
-    output += str(quote['regularMarketPrice']) + " (" 
-    if quote['regularMarketChangePercent'] > 0:
-        output += "+"
-    output += str(round(quote['regularMarketChangePercent'], 2)) + "%)"
-  return(output)
+    if quote["marketState"] == "PRE":
+        output += "[PRE] " + str(quote["preMarketPrice"]) + " ("
+        if quote["preMarketChangePercent"] > 0:
+            output += "+"
+        output += str(round(quote["preMarketChangePercent"], 2)) + "%)"
+    elif quote["marketState"] == "POST":
+        output += "[POST] " + str(quote["postMarketPrice"]) + " ("
+        if quote["postMarketChangePercent"] > 0:
+            output += "+"
+        output += str(round(quote["postMarketChangePercent"], 2)) + "%)"
+    else:
+        output += str(quote["regularMarketPrice"]) + " ("
+        if quote["regularMarketChangePercent"] > 0:
+            output += "+"
+        output += str(round(quote["regularMarketChangePercent"], 2)) + "%)"
+    return output
+
 
 @client.event
 async def on_ready():
-  print('Logged in as {0.user}'.format(client))
+    print("Logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-  # Prevent messages from the bot itself
-  if message.author == client.user:
-    return
 
-  # If channel protection is enabled, check if right channel
-  if channel_protection and str(message.channel) != str(bot_channel):
-    return
+@slash.slash(
+    name="stock",
+    description="Get the current price of a given stock",
+    options=[
+        create_option(
+            name="ticker",
+            description="Stock ticker (e.g. GME)",
+            option_type=3,  # string
+            required=True,
+        )
+    ],
+)
+async def stock(ctx, ticker: str):
+    await ctx.send(f"{get_quote(ticker)}")
 
-  # Listen only for correct commands
-  if not message.content.startswith(command):
-    return
-  
-  # Check that we get only one symbol
-  if len(message.content.split()) != 2:
-    answer = "Error. Only one stock supported at the moment. Usage: `$stonk SYMBOL`"
-  else:
-    answer = get_quote(message.content.split()[1])
 
-    # Check that the symbol is actually valid
-    if answer == False:
-      answer = "Error: unknown symbol"
-
-  await message.channel.send(answer)
-
-client.run(os.getenv('DISC_BOT_TOKEN'))
+client.run(os.getenv("STONKSBOT_TOKEN"))
